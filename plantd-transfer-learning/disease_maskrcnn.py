427cc10fe75ffd5f7b3a4f6cc54f8a9c513536e9
DESKTOP-88REMCB\Admin <kate.attard.2001@gmail.com>
2024-09-14 11:02:57 +0200

    removing comments

--- plantd-transfer-learning/disease_maskrcnn.py
+++ plantd-transfer-learning/disease_maskrcnn.py
@@ -16,10 +16,9 @@ import glob
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 tf.get_logger().setLevel('WARNING')
 
-# Directories
-DATASET_DIR = os.path.join(os.getcwd(), "data")  # Volume where the images wind up
-MASKRCNN_DIR = os.path.join(os.getcwd(), "maskrcnn")  # Volume where we will keep logs/models(weights)
-MODEL_DIR = os.path.join(MASKRCNN_DIR, "models")  # Model directory within the mrcnn volume
+DATASET_DIR = os.path.join(os.getcwd(), "data")
+MASKRCNN_DIR = os.path.join(os.getcwd(), "maskrcnn")
+MODEL_DIR = os.path.join(MASKRCNN_DIR, "models")
 LOG_DIR = os.path.join(MASKRCNN_DIR, "logs")
 WEIGHTS_DIR = os.path.join(MODEL_DIR, "weights")
 BEST_MODEL_PATH = os.path.join(WEIGHTS_DIR, "best_model.h5")
@@ -31,15 +30,12 @@ if not os.path.exists(LOG_DIR):
 if not os.path.exists(WEIGHTS_DIR):
     os.makedirs(WEIGHTS_DIR)
 
-# Useful vars
 total_epochs = 100
 total_train_images = len([name for name in os.listdir(os.path.join(DATASET_DIR, "train", "images")) if os.path.isfile(os.path.join(os.path.join(DATASET_DIR, "train", "images"), name)) and name.endswith('.jpg')])
 total_val_images = len([name for name in os.listdir(os.path.join(DATASET_DIR, "val", "images")) if os.path.isfile(os.path.join(os.path.join(DATASET_DIR, "val", "images"), name)) and name.endswith('.jpg')])
 
 
-# Utility functions to read/write the last completed epoch and best validation loss
 def read_last_epoch(file_path):
-    """Reads the last completed epoch from the file."""
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             return int(f.read().strip())
@@ -47,34 +43,28 @@ def read_last_epoch(file_path):
 
 
 def write_last_epoch(file_path, epoch):
-    """Writes the last completed epoch to the file."""
     with open(file_path, 'w') as f:
         f.write(str(epoch))
 
 
 def read_best_val_loss(file_path):
-    """Reads the best validation loss from the file."""
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             return float(f.read().strip())
-    return np.inf  # If no file exists, return infinity to ensure any new loss is better
+    return np.inf
 
 
 def write_best_val_loss(file_path, val_loss):
-    """Writes the best validation loss to the file."""
     with open(file_path, 'w') as f:
         f.write(str(val_loss))
 
 
-# Set initial_epoch and best_val_loss from files
 initial_epoch = read_last_epoch(EPOCH_FILE_PATH)
 best_val_loss = read_best_val_loss(BEST_VAL_LOSS_FILE_PATH)
 
 
-# Datasets setup
 class DiseaseDataset(mrcnn.utils.Dataset):
     def load_dataset(self, dataset_dir, subset):
-        """Load a subset of the Plant Disease dataset."""
         self.add_class("dataset", 1, "plant_disease")
 
         subset_dir = os.path.join(dataset_dir, subset)
@@ -89,17 +79,15 @@ class DiseaseDataset(mrcnn.utils.Dataset):
             self.add_image("dataset", image_id=image_id, path=image_path, mask_path=mask_path)
 
     def load_mask(self, image_id):
-        """Generate instance masks for an image."""
         info = self.image_info[image_id]
         mask_path = info['mask_path']
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-        mask = (mask > 0).astype(np.uint8)  # Convert to binary mask
-        mask = np.expand_dims(mask, axis=-1)  # Add an extra dimension
+        mask = (mask > 0).astype(np.uint8)
+        mask = np.expand_dims(mask, axis=-1)
         class_ids = np.array([1], dtype=np.int32)
         return mask, class_ids
 
 
-# Configurations
 class TrainConfig(mrcnn.config.Config):
     NAME = "train_cfg"
     GPU_COUNT = 1
@@ -120,7 +108,6 @@ class InferConfig(TrainConfig):
 
 inference_config = InferConfig()
 
-# Dataset Preparation
 dataset_train = DiseaseDataset()
 dataset_train.load_dataset(DATASET_DIR, "train")
 dataset_train.prepare()
@@ -129,16 +116,14 @@ dataset_val = DiseaseDataset()
 dataset_val.load_dataset(DATASET_DIR, "val")
 dataset_val.prepare()
 
-# Model Initialization
 model = mrcnn.model.MaskRCNN(mode="training", config=training_config, model_dir=MODEL_DIR)
 
-# Custom Callback for Metrics and Model Checkpointing
 class MetricsCallback(tf.keras.callbacks.Callback):
     def __init__(self, log_file=os.path.join(LOG_DIR, "maskrcnn.log")):
         super(MetricsCallback, self).__init__()
         self.log_file = log_file
         self.start_time = None
-        self.best_val_loss = best_val_loss  # Initialize with the loaded best validation loss
+        self.best_val_loss = best_val_loss
         self.best_checkpoint_path = BEST_MODEL_PATH
 
         if not os.path.exists(self.log_file):
@@ -151,12 +136,8 @@ class MetricsCallback(tf.keras.callbacks.Callback):
         formatted_start_time = self.start_time.strftime("%Y-%m-%d %H:%M:%S")
         print(f"Training started at: {formatted_start_time}")
 
-        #with open(self.log_file, 'a') as f:
-            #f.write(f"{formatted_start_time},N/A,N/A,N/A,N/A,N/A,N/A,N/A,N/A,N/A\n")
-
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
-        # Update the epoch file
         write_last_epoch(EPOCH_FILE_PATH, read_last_epoch(EPOCH_FILE_PATH) + 1)
         print(f'\nEpoch {epoch + 1} Metrics:')
         end_time = datetime.now()
@@ -168,7 +149,6 @@ class MetricsCallback(tf.keras.callbacks.Callback):
         self.model.save_weights(model_path)
 
 
-        # Evaluate model on validation data and compute custom metrics
         inference_model = mrcnn.model.MaskRCNN(mode='inference',
                                                model_dir=MODEL_DIR,
                                                config=inference_config)
@@ -238,21 +218,18 @@ class MetricsCallback(tf.keras.callbacks.Callback):
             print(f'{metric_name}: {metric_value:.4f}')
 
 
-# Training Logic with Mask R-CNN Setup
 metrics_callback = MetricsCallback()
 
-# Load the best model checkpoint if it exists
 if os.path.exists(BEST_MODEL_PATH):
     print(f"Loading best model weights from: {BEST_MODEL_PATH}")
     model.load_weights(BEST_MODEL_PATH, by_name=True)
 else:
     print("No best model found, starting from scratch.")
 
-# Manually control the training loop to restart from a specific epoch
 for epoch in range(initial_epoch, total_epochs):
     print(f"epoch: {epoch}, init epoch: {initial_epoch}, total epoch: {total_epochs}")
     model.train(dataset_train, dataset_val,
                 learning_rate=training_config.LEARNING_RATE,
-                epochs=total_epochs-initial_epoch,  # Incrementally increase the epoch count
+                epochs=total_epochs-initial_epoch,
                 layers='heads',
                 custom_callbacks=[metrics_callback])
